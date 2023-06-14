7b0df4d37d42ec491e4daec1afa8fc21faa1f8e0
CatsPsychologist <mainofficee@gmail.com>
2023-06-14 18:56:20 +0300

    changed types in service

--- test-app/src/app/shared/services/time-transform.service.ts
+++ test-app/src/app/shared/services/time-transform.service.ts
@@ -7,7 +7,7 @@ export class TimeTransformService {
 
   constructor() { }
 
-  getTime(ms : any, s : any, m : any, h : any){
+  getTime(ms : number , s : number | string, m : number | string, h : number | string){
 
     s = ms % 60;
     m = Math.floor(ms / 60) % 60;
@@ -26,6 +26,5 @@ export class TimeTransformService {
     // this.s = this.s < 10 ? '0' + this.s : this.s;
     // this.m = this.m < 10 ? '0' + this.m : this.m;
     // this.h = this.h < 10 ? '0' + this.h : this.h;
-
 
 }
